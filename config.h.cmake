0e5dbd8825ee1fb51e618b9725dc33c70506c12b
hanepjiv <hanepjiv@gmail.com>
2015-08-11 21:45:06 +0900

    update

--- config.h.cmake
+++ config.h.cmake
@@ -15,19 +15,19 @@
 #ifndef @SNAPKIT_PACKAGE_UPPER@_CONFIG_H_
 #define @SNAPKIT_PACKAGE_UPPER@_CONFIG_H_
 
+#define PACKAGE                 "@SNAPKIT_PACKAGE@"
+#define PACKAGE_BUGREPORT       "@SNAPKIT_BUGREPORT@"
+#define PACKAGE_NAME            "@SNAPKIT_PACKAGE@"
+#define PACKAGE_STRING          "@SNAPKIT_PACKAGE@ @SNAPKIT_RELEASE@"
+#define PACKAGE_TARNAME         "@SNAPKIT_PACKAGE@"
+#define PACKAGE_URL             "@SNAPKIT_URL@"
+#define PACKAGE_VERSION         "@SNAPKIT_RELEASE@"
+#define VERSION                 "@SNAPKIT_RELEASE@"
+
 #if @Boost_FOUND@
 #  define HAVE_BOOST 1
 #else
 #  undef HAVE_BOOST
 #endif
-
-#define PACKAGE                         "@SNAPKIT_PACKAGE@"
-#define PACKAGE_BUGREPORT               "@SNAPKIT_BUGREPORT@"
-#define PACKAGE_NAME                    "@SNAPKIT_PACKAGE@"
-#define PACKAGE_STRING                  "@SNAPKIT_PACKAGE@ @SNAPKIT_RELEASE@"
-#define PACKAGE_TARNAME                 "@SNAPKIT_PACKAGE@"
-#define PACKAGE_URL                     "@SNAPKIT_URL@"
-#define PACKAGE_VERSION                 "@SNAPKIT_RELEASE@"
-#define VERSION                         "@SNAPKIT_RELEASE@"
 
 #endif  /* @SNAPKIT_PACKAGE_UPPER@_CONFIG_H_ */

fabc919a549cd7f77330f7166f381441d1edace7
hanepjiv <hanepjiv@gmail.com>
2015-08-11 20:04:09 +0900

    config.h

---
+++ config.h.cmake
@@ -0,0 +1,33 @@
+/** -*- coding:utf-8; mode:c; -*-
+ *  @file config.h.in
+ *  @brief config.h.in
+ *
+ *  config.h.in
+ *
+ *  Copyright 2015 hanepjiv
+ *
+ *  @author hanepjiv <hagumo@gmail.com>
+ *  @since 2015/08/08
+ *  @date 2015/08/11
+ */
+
+
+#ifndef @SNAPKIT_PACKAGE_UPPER@_CONFIG_H_
+#define @SNAPKIT_PACKAGE_UPPER@_CONFIG_H_
+
+#if @Boost_FOUND@
+#  define HAVE_BOOST 1
+#else
+#  undef HAVE_BOOST
+#endif
+
+#define PACKAGE                         "@SNAPKIT_PACKAGE@"
+#define PACKAGE_BUGREPORT               "@SNAPKIT_BUGREPORT@"
+#define PACKAGE_NAME                    "@SNAPKIT_PACKAGE@"
+#define PACKAGE_STRING                  "@SNAPKIT_PACKAGE@ @SNAPKIT_RELEASE@"
+#define PACKAGE_TARNAME                 "@SNAPKIT_PACKAGE@"
+#define PACKAGE_URL                     "@SNAPKIT_URL@"
+#define PACKAGE_VERSION                 "@SNAPKIT_RELEASE@"
+#define VERSION                         "@SNAPKIT_RELEASE@"
+
+#endif  /* @SNAPKIT_PACKAGE_UPPER@_CONFIG_H_ */

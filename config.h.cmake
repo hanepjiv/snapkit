d26290f1d8bfd6472ed67e319af7e6fedcddf907
hanepjiv <hanepjiv@gmail.com>
2015-09-25 00:21:16 +0900

    RELEASE -> VERSION

--- config.h.cmake
+++ config.h.cmake
@@ -8,7 +8,7 @@
  *
  *  @author hanepjiv <hagumo@gmail.com>
  *  @since 2015/08/08
- *  @date 2015/08/11
+ *  @date 2015/09/25
  */
 
 
@@ -18,11 +18,11 @@
 #define PACKAGE                 "@SNAPKIT_PACKAGE@"
 #define PACKAGE_BUGREPORT       "@SNAPKIT_BUGREPORT@"
 #define PACKAGE_NAME            "@SNAPKIT_PACKAGE@"
-#define PACKAGE_STRING          "@SNAPKIT_PACKAGE@ @SNAPKIT_RELEASE@"
+#define PACKAGE_STRING          "@SNAPKIT_PACKAGE@ @SNAPKIT_VERSION@"
 #define PACKAGE_TARNAME         "@SNAPKIT_PACKAGE@"
 #define PACKAGE_URL             "@SNAPKIT_URL@"
-#define PACKAGE_VERSION         "@SNAPKIT_RELEASE@"
-#define VERSION                 "@SNAPKIT_RELEASE@"
+#define PACKAGE_VERSION         "@SNAPKIT_VERSION@"
+#define VERSION                 "@SNAPKIT_VERSION@"
 
 #if @Boost_FOUND@
 #  define HAVE_BOOST 1

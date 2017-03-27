51bb28f91f1071a4068b3aefc40cfaf089428b78
hanepjiv <hanepjiv@gmail.com>
2017-03-27 23:44:16 +0900

    column79

--- Modules/CCxxFlags.cmake
+++ Modules/CCxxFlags.cmake
@@ -1,6 +1,6 @@
-# ##############################################################################
-# COMPILER  ====================================================================
-# None -------------------------------------------------------------------------
+# #############################################################################
+# COMPILER  ===================================================================
+# None ------------------------------------------------------------------------
 set(COMPILER_C_CXX_FLAGS "")
 set(COMPILER_C_FLAGS "")
 set(COMPILER_CXX_FLAGS "")
@@ -8,7 +8,7 @@ set(COMPILER_EXE_LINKER_FLAGS "")
 set(COMPILER_MODULE_LINKER_FLAGS "")
 set(COMPILER_SHARED_LINKER_FLAGS "")
 set(COMPILER_STATIC_LINKER_FLAGS "")
-# DEGUG ------------------------------------------------------------------------
+# DEGUG -----------------------------------------------------------------------
 set(COMPILER_C_CXX_FLAGS_DEBUG "")
 set(COMPILER_C_FLAGS_DEBUG "")
 set(COMPILER_CXX_FLAGS_DEBUG "")
@@ -16,7 +16,7 @@ set(COMPILER_EXE_LINKER_FLAGS_DEBUG "")
 set(COMPILER_MODULE_LINKER_FLAGS_DEBUG "")
 set(COMPILER_SHARED_LINKER_FLAGS_DEBUG "")
 set(COMPILER_STATIC_LINKER_FLAGS_DEBUG "")
-# RELEASE-----------------------------------------------------------------------
+# RELEASE----------------------------------------------------------------------
 set(COMPILER_C_CXX_FLAGS_RELEASE "")
 set(COMPILER_C_FLAGS_RELEASE "")
 set(COMPILER_CXX_FLAGS_RELEASE "")
@@ -24,7 +24,7 @@ set(COMPILER_EXE_LINKER_FLAGS_RELEASE "")
 set(COMPILER_MODULE_LINKER_FLAGS_RELEASE "")
 set(COMPILER_SHARED_LINKER_FLAGS_RELEASE "")
 set(COMPILER_STATIC_LINKER_FLAGS_RELEASE "")
-# RELWITHDEBINFO ---------------------------------------------------------------
+# RELWITHDEBINFO --------------------------------------------------------------
 set(COMPILER_C_CXX_FLAGS_RELWITHDEBINFO "")
 set(COMPILER_C_FLAGS_RELWITHDEBINFO "")
 set(COMPILER_CXX_FLAGS_RELWITHDEBINFO "")
@@ -32,7 +32,7 @@ set(COMPILER_EXE_LINKER_FLAGS_RELWITHDEBINFO "")
 set(COMPILER_MODULE_LINKER_FLAGS_RELWITHDEBINFO "")
 set(COMPILER_SHARED_LINKER_FLAGS_RELWITHDEBINFO "")
 set(COMPILER_STATIC_LINKER_FLAGS_RELWITHDEBINFO "")
-# MINSIZEREL--------------------------------------------------------------------
+# MINSIZEREL-------------------------------------------------------------------
 set(COMPILER_C_CXX_FLAGS_MINSIZEREL "")
 set(COMPILER_C_FLAGS_MINSIZEREL "")
 set(COMPILER_CXX_FLAGS_MINSIZEREL "")
@@ -40,11 +40,11 @@ set(COMPILER_EXE_LINKER_FLAGS_MINSIZEREL "")
 set(COMPILER_MODULE_LINKER_FLAGS_MINSIZEREL "")
 set(COMPILER_SHARED_LINKER_FLAGS_MINSIZEREL "")
 set(COMPILER_STATIC_LINKER_FLAGS_MINSIZEREL "")
-# ##############################################################################
-# compiler  ====================================================================
+# #############################################################################
+# compiler  ===================================================================
 if (CMAKE_COMPILER_IS_GNUCC OR
-    CMAKE_C_COMPILER_ID STREQUAL "Clang")       # GCC or Clang  ================
-  # Common  --------------------------------------------------------------------
+    CMAKE_C_COMPILER_ID STREQUAL "Clang")       # GCC or Clang  ===============
+  # Common  -------------------------------------------------------------------
   if (UNIX)
     set(COMPILER_C_CXX_FLAGS "${COMPILER_C_CXX_FLAGS} -fPIC")
     set(COMPILER_SHARED_LINKER_FLAGS "${COMPILER_SHARED_LINKER_FLAGS} -Wl,-z,defs")
@@ -57,114 +57,114 @@ if (CMAKE_COMPILER_IS_GNUCC OR
   set(COMPILER_C_FLAGS "${COMPILER_C_FLAGS} -Wbad-function-cast -Wstrict-prototypes -Wold-style-definition -Wmissing-prototypes -Wnested-externs -Wdeclaration-after-statement")
   set(COMPILER_CXX_FLAGS "${COMPILER_CXX_FLAGS} -fvisibility-inlines-hidden -fthreadsafe-statics -Weffc++ -Woverloaded-virtual -Wsign-promo -Wsynth")
   set(COMPILER_SHARED_LINKER_FLAGS "${COMPILER_SHARED_LINKER_FLAGS} -shared")
-  # Debug  ---------------------------------------------------------------------
+  # Debug  --------------------------------------------------------------------
   set(COMPILER_C_CXX_FLAGS_DEBUG "${COMPILER_C_CXX_FLAGS_DEBUG} -O0 -g -fverbose-asm")
-  # Release  -------------------------------------------------------------------
+  # Release  ------------------------------------------------------------------
   set(COMPILER_C_CXX_FLAGS_RELEASE "${COMPILER_C_CXX_FLAGS_RELEASE} -O3 -fomit-frame-pointer")
-  # RelWithDebInfo  ------------------------------------------------------------
+  # RelWithDebInfo  -----------------------------------------------------------
   set(COMPILER_C_CXX_FLAGS_RELWITHDEBINFO "${COMPILER_C_CXX_FLAGS_RELWITHDEBINFO} -O2 -g -fverbose-asm")
-  # MinSizeRel  ----------------------------------------------------------------
+  # MinSizeRel  ---------------------------------------------------------------
   set(COMPILER_C_CXX_FLAGS_MINSIZEREL "${COMPILER_C_CXX_FLAGS_MINSIZEREL} -Os -fomit-frame-pointer")
 endif()
-if (CMAKE_COMPILER_IS_GNUCC)                    # GCC  =========================
-  # Common  --------------------------------------------------------------------
+if (CMAKE_COMPILER_IS_GNUCC)                    # GCC  ========================
+  # Common  -------------------------------------------------------------------
   #  pass
-  # Debug  ---------------------------------------------------------------------
+  # Debug  --------------------------------------------------------------------
   set(COMPILER_C_CXX_FLAGS_DEBUG "${COMPILER_C_CXX_FLAGS_DEBUG} -pg -ftrapv")
   set(COMPILER_EXE_LINKER_FLAGS_DEBUG "${COMPILER_EXE_LINKER_FLAGS_DEBUG} -pg")
   set(COMPILER_MODULE_LINKER_FLAGS_DEBUG "${COMPILER_MODULE_LINKER_FLAGS_DEBUG} -pg")
-  # Release  -------------------------------------------------------------------
+  # Release  ------------------------------------------------------------------
   #   pass
-  # RelWithDebInfo  ------------------------------------------------------------
+  # RelWithDebInfo  -----------------------------------------------------------
   set(COMPILER_C_CXX_FLAGS_RELWITHDEBINFO "${COMPILER_C_CXX_FLAGS_RELWITHDEBINFO} ${COMPILER_C_CXX_FLAGS_DEBUG}")
   set(COMPILER_EXE_LINKER_FLAGS_RELWITHDEBINFO "${COMPILER_EXE_LINKER_FLAGS_RELWITHDEBINFO} ${COMPILER_EXE_LINKER_FLAGS_DEBUG}")
   set(COMPILER_MODULE_LINKER_FLAGS_RELWITHDEBINFO "${COMPILER_MODULE_LINKER_FLAGS_RELWITHDEBINFO} ${COMPILER_MODULE_LINKER_FLAGS_RELWITHDEBINFO}")
-  # MinSizeRel  ----------------------------------------------------------------
+  # MinSizeRel  ---------------------------------------------------------------
   #   pass
-elseif(CMAKE_C_COMPILER_ID STREQUAL "Clang")    # Clang  =======================
-  # Common  --------------------------------------------------------------------
+elseif(CMAKE_C_COMPILER_ID STREQUAL "Clang")    # Clang  ======================
+  # Common  -------------------------------------------------------------------
   set(COMPILER_C_FLAGS "${COMPILER_C_FLAGS} -fsanitize-undefined-trap-on-error")
-  # Debug  ---------------------------------------------------------------------
+  # Debug  --------------------------------------------------------------------
   #   pass
-  # Release  -------------------------------------------------------------------
+  # Release  ------------------------------------------------------------------
   #   pass
-  # RelWithDebInfo  ------------------------------------------------------------
+  # RelWithDebInfo  -----------------------------------------------------------
   #   pass
-  # MinSizeRel  ----------------------------------------------------------------
+  # MinSizeRel  ---------------------------------------------------------------
   #   pass
-elseif(CMAKE_C_COMPILER_ID STREQUAL "Intel")    # Intel  =======================
+elseif(CMAKE_C_COMPILER_ID STREQUAL "Intel")    # Intel  ======================
   message(FATAL_ERROR "Intel is not yet supported.")
-elseif(CMAKE_C_COMPILER_ID STREQUAL "MSVC")     # MSVC  ========================
+elseif(CMAKE_C_COMPILER_ID STREQUAL "MSVC")     # MSVC  =======================
   message(FATAL_ERROR "MSVC is not yet supported.")
 endif()
-# ##############################################################################
-# USER  ========================================================================
-# Common  ----------------------------------------------------------------------
+# #############################################################################
+# USER  =======================================================================
+# Common  ---------------------------------------------------------------------
 set(USER_C_FLAGS "${COMPILER_C_CXX_FLAGS} ${COMPILER_C_FLAGS}" CACHE STRING "")
 set(USER_CXX_FLAGS "${COMPILER_C_CXX_FLAGS} ${COMPILER_CXX_FLAGS}" CACHE STRING "")
 set(USER_EXE_LINKER_FLAGS "${COMPILER_EXE_LINKER_FLAGS}" CACHE STRING "")
 set(USER_MODULE_LINKER_FLAGS "${COMPILER_MODULE_LINKER_FLAGS}" CACHE STRING "")
 set(USER_SHARED_LINKER_FLAGS "${COMPILER_SHARED_LINKER_FLAGS}" CACHE STRING "")
 set(USER_STATIC_LINKER_FLAGS "${COMPILER_STATIC_LINKER_FLAGS}" CACHE STRING "")
-# Debug  -----------------------------------------------------------------------
+# Debug  ----------------------------------------------------------------------
 set(USER_C_FLAGS_DEBUG "${COMPILER_C_CXX_FLAGS_DEBUG} ${COMPILER_C_FLAGS_DEBUG}" CACHE STRING "")
 set(USER_CXX_FLAGS_DEBUG "${COMPILER_C_CXX_FLAGS_DEBUG} ${COMPILER_CXX_FLAGS_DEBUG}" CACHE STRING "")
 set(USER_EXE_LINKER_FLAGS_DEBUG "${COMPILER_EXE_LINKER_FLAGS_DEBUG}" CACHE STRING "")
 set(USER_MODULE_LINKER_FLAGS_DEBUG "${COMPILER_MODULE_LINKER_FLAGS_DEBUG}" CACHE STRING "")
 set(USER_SHARED_LINKER_FLAGS_DEBUG "${COMPILER_SHARED_LINKER_FLAGS_DEBUG}" CACHE STRING "")
 set(USER_STATIC_LINKER_FLAGS_DEBUG "${COMPILER_STATIC_LINKER_FLAGS_DEBUG}" CACHE STRING "")
-# Release  -------------------------------------------------------------------
+# Release  --------------------------------------------------------------------
 set(USER_C_FLAGS_RELEASE "${COMPILER_C_CXX_FLAGS_RELEASE} ${COMPILER_C_FLAGS_RELEASE}" CACHE STRING "")
 set(USER_CXX_FLAGS_RELEASE "${COMPILER_C_CXX_FLAGS_RELEASE} ${COMPILER_CXX_FLAGS_RELEASE}" CACHE STRING "")
 set(USER_EXE_LINKER_FLAGS_RELEASE "${COMPILER_EXE_LINKER_FLAGS_RELEASE}" CACHE STRING "")
 set(USER_MODULE_LINKER_FLAGS_RELEASE "${COMPILER_MODULE_LINKER_FLAGS_RELEASE}" CACHE STRING "")
 set(USER_SHARED_LINKER_FLAGS_RELEASE "${COMPILER_SHARED_LINKER_FLAGS_RELEASE}" CACHE STRING "")
 set(USER_STATIC_LINKER_FLAGS_RELEASE "${COMPILER_STATIC_LINKER_FLAGS_RELEASE}" CACHE STRING "")
-# RelWithDebInfo  --------------------------------------------------------------
+# RelWithDebInfo  -------------------------------------------------------------
 set(USER_C_FLAGS_RELWITHDEBINFO "${COMPILER_C_CXX_FLAGS_RELWITHDEBINFO} ${COMPILER_C_FLAGS_RELWITHDEBINFO}" CACHE STRING "")
 set(USER_CXX_FLAGS_RELWITHDEBINFO "${COMPILER_C_CXX_FLAGS_RELWITHDEBINFO} ${COMPILER_CXX_FLAGS_RELWITHDEBINFO}" CACHE STRING "")
 set(USER_EXE_LINKER_FLAGS_RELWITHDEBINFO "${COMPILER_EXE_LINKER_FLAGS_RELWITHDEBINFO}" CACHE STRING "")
 set(USER_MODULE_LINKER_FLAGS_RELWITHDEBINFO "${COMPILER_MODULE_LINKER_FLAGS_RELWITHDEBINFO}" CACHE STRING "")
 set(USER_SHARED_LINKER_FLAGS_RELWITHDEBINFO "${COMPILER_SHARED_LINKER_FLAGS_RELWITHDEBINFO}" CACHE STRING "")
 set(USER_STATIC_LINKER_FLAGS_RELWITHDEBINFO "${COMPILER_STATIC_LINKER_FLAGS_RELWITHDEBINFO}" CACHE STRING "")
-# MinSizeRel  ------------------------------------------------------------------
+# MinSizeRel  -----------------------------------------------------------------
 set(USER_C_FLAGS_MINSIZEREL "${COMPILER_C_CXX_FLAGS_MINSIZEREL} ${COMPILER_C_FLAGS_MINSIZEREL}" CACHE STRING "")
 set(USER_CXX_FLAGS_MINSIZEREL "${COMPILER_C_CXX_FLAGS_MINSIZEREL} ${COMPILER_CXX_FLAGS_MINSIZEREL}" CACHE STRING "")
 set(USER_EXE_LINKER_FLAGS_MINSIZEREL "${COMPILER_EXE_LINKER_FLAGS_MINSIZEREL}" CACHE STRING "")
 set(USER_MODULE_LINKER_FLAGS_MINSIZEREL "${COMPILER_MODULE_LINKER_FLAGS_MINSIZEREL}" CACHE STRING "")
 set(USER_SHARED_LINKER_FLAGS_MINSIZEREL "${COMPILER_SHARED_LINKER_FLAGS_MINSIZEREL}" CACHE STRING "")
 set(USER_STATIC_LINKER_FLAGS_MINSIZEREL "${COMPILER_STATIC_LINKER_FLAGS_MINSIZEREL}" CACHE STRING "")
-# ##############################################################################
-# CMAKE  =======================================================================
-# Common  ----------------------------------------------------------------------
+# #############################################################################
+# CMAKE  ======================================================================
+# Common  ---------------------------------------------------------------------
 set(CMAKE_C_FLAGS "${USER_C_FLAGS}" CACHE INTERNAL "")
 set(CMAKE_CXX_FLAGS "${USER_CXX_FLAGS}" CACHE INTERNAL "")
 set(CMAKE_EXE_LINKER_FLAGS "${USER_EXE_LINKER_FLAGS}" CACHE INTERNAL "")
 set(CMAKE_MODULE_LINKER_FLAGS "${USER_MODULE_LINKER_FLAGS}" CACHE INTERNAL "")
 set(CMAKE_SHARED_LINKER_FLAGS "${USER_SHARED_LINKER_FLAGS}" CACHE INTERNAL "")
 set(CMAKE_STATIC_LINKER_FLAGS "${USER_STATIC_LINKER_FLAGS}" CACHE INTERNAL "")
-# Debug  -----------------------------------------------------------------------
+# Debug  ----------------------------------------------------------------------
 set(CMAKE_C_FLAGS_DEBUG "${USER_C_FLAGS_DEBUG}" CACHE INTERNAL "")
 set(CMAKE_CXX_FLAGS_DEBUG "${USER_C_CXX_FLAGS_DEBUG} ${USER_CXX_FLAGS_DEBUG}" CACHE INTERNAL "")
 set(CMAKE_EXE_LINKER_FLAGS_DEBUG "${USER_EXE_LINKER_FLAGS_DEBUG}" CACHE INTERNAL "")
 set(CMAKE_MODULE_LINKER_FLAGS_DEBUG "${USER_MODULE_LINKER_FLAGS_DEBUG}" CACHE INTERNAL "")
 set(CMAKE_SHARED_LINKER_FLAGS_DEBUG "${USER_SHARED_LINKER_FLAGS_DEBUG}" CACHE INTERNAL "")
 set(CMAKE_STATIC_LINKER_FLAGS_DEBUG "${USER_STATIC_LINKER_FLAGS_DEBUG}" CACHE INTERNAL "")
-# Release  -------------------------------------------------------------------
+# Release  --------------------------------------------------------------------
 set(CMAKE_C_FLAGS_RELEASE "${USER_C_FLAGS_RELEASE}" CACHE INTERNAL "")
 set(CMAKE_CXX_FLAGS_RELEASE "${USER_C_CXX_FLAGS_RELEASE} ${USER_CXX_FLAGS_RELEASE}" CACHE INTERNAL "")
 set(CMAKE_EXE_LINKER_FLAGS_RELEASE "${USER_EXE_LINKER_FLAGS_RELEASE}" CACHE INTERNAL "")
 set(CMAKE_MODULE_LINKER_FLAGS_RELEASE "${USER_MODULE_LINKER_FLAGS_RELEASE}" CACHE INTERNAL "")
 set(CMAKE_SHARED_LINKER_FLAGS_RELEASE "${USER_SHARED_LINKER_FLAGS_RELEASE}" CACHE INTERNAL "")
 set(CMAKE_STATIC_LINKER_FLAGS_RELEASE "${USER_STATIC_LINKER_FLAGS_RELEASE}" CACHE INTERNAL "")
-# RelWithDebInfo  --------------------------------------------------------------
+# RelWithDebInfo  -------------------------------------------------------------
 set(CMAKE_C_FLAGS_RELWITHDEBINFO "${USER_C_FLAGS_RELWITHDEBINFO}" CACHE INTERNAL "")
 set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${USER_CXX_FLAGS_RELWITHDEBINFO}" CACHE INTERNAL "")
 set(CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO "${USER_EXE_LINKER_FLAGS_RELWITHDEBINFO}" CACHE INTERNAL "")
 set(CMAKE_MODULE_LINKER_FLAGS_RELWITHDEBINFO "${USER_MODULE_LINKER_FLAGS_RELWITHDEBINFO}" CACHE INTERNAL "")
 set(CMAKE_SHARED_LINKER_FLAGS_RELWITHDEBINFO "${USER_SHARED_LINKER_FLAGS_RELWITHDEBINFO}" CACHE INTERNAL "")
 set(CMAKE_STATIC_LINKER_FLAGS_RELWITHDEBINFO "${USER_STATIC_LINKER_FLAGS_RELWITHDEBINFO}" CACHE INTERNAL "")
-# MinSizeRel  ------------------------------------------------------------------
+# MinSizeRel  -----------------------------------------------------------------
 set(CMAKE_C_FLAGS_MINSIZEREL "${USER_C_FLAGS_MINSIZEREL}" CACHE INTERNAL "")
 set(CMAKE_CXX_FLAGS_MINSIZEREL "${USER_CXX_FLAGS_MINSIZEREL}" CACHE INTERNAL "")
 set(CMAKE_EXE_LINKER_FLAGS_MINSIZEREL "${USER_EXE_LINKER_FLAGS_MINSIZEREL}" CACHE INTERNAL "")

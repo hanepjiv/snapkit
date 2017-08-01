7c24cc53e05319d9ed7aea0990333c6b35ef014a
hanepjiv <hanepjiv@gmail.com>
2017-08-01 18:44:20 +0900

    update

--- src/debug.h
+++ src/debug.h
@@ -9,7 +9,7 @@
  *  @author hanepjiv <hanepjiv@gmail.com>
  *  @copyright The MIT License (MIT) / Apache License Version 2.0
  *  @since %%TODAY%%
- *  @date 2017/06/29
+ *  @date 2017/08/01
  */
 
 #ifndef SNAPKIT_DEBUG_H_
@@ -17,52 +17,67 @@
 
 /* ///////////////////////////////////////////////////////////////////////// */
 /* ========================================================================= */
-# ifdef __cplusplus
-#   include <cstdio>
-#   include <cstdlib>
-#   include <cstdarg>
-#   define SNAPKIT_USING_NAMESPACE_STD using namespace std
-# else  /* !__cplusplus */
-#   include <stdio.h>
-#   include <stdlib.h>
-#   include <stdarg.h>
-#   define SNAPKIT_USING_NAMESPACE_STD
-# endif  /* !__cplusplus */
+#define SNAPKIT_VERBOSITY_DEBUG                 (0x00)
+#define SNAPKIT_VERBOSITY_INFO                  (0x3F)
+#define SNAPKIT_VERBOSITY_WARNING               (0x7F)
+#define SNAPKIT_VERBOSITY_ERROR                 (0xBF)SNAPKIT_VERBOSITY_03
+#define SNAPKIT_VERBOSITY_CRITICAL              (0xFF)SNAPKIT_VERBOSITY_04
+/* ------------------------------------------------------------------------- */
+#define SNAPKIT_VERBOSITY_ALL                   SNAPKIT_VERBOSITY_DEBUG
+#define SNAPKIT_VERBOSITY_DEFAULT               SNAPKIT_VERBOSITY_INFO
+#define SNAPKIT_VERBOSITY_QUIET                 SNAPKIT_VERBOSITY_CRITICAL
+/* ------------------------------------------------------------------------- */
+#if !defined(SNAPKIT_VERBOSITY)
+# define SNAPKIT_VERBOSITY                      SNAPKIT_VERBOSITY_DEFAULT
+#endif
+/* ///////////////////////////////////////////////////////////////////////// */
 /* ========================================================================= */
-# define SNAPKIT_TRACE_ALWAYS__(d)                              \
-  do {                                                          \
-    SNAPKIT_USING_NAMESPACE_STD;                                \
-    fprintf(stderr, "%s(%i) : " d "\n", __FILE__, __LINE__);    \
-    (void) fflush(stderr);                                      \
+#ifdef __cplusplus
+# include <cstdio>
+# include <cstdlib>
+# include <cstdarg>
+# define SNAPKIT_USING_NAMESPACE_STD using namespace std
+#else  /* !__cplusplus */
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# define SNAPKIT_USING_NAMESPACE_STD
+#endif  /* !__cplusplus */
+/* ========================================================================= */
+#define SNAPKIT_TRACE_ALWAYS__(d, line)                                 \
+  do {                                                                  \
+    SNAPKIT_USING_NAMESPACE_STD;                                        \
+    (void) fputs(PACKAGE_STRING " : " __FILE__ "(" #line ") : " d "\n", \
+                 stderr);                                               \
+    (void) fflush(stderr);                                              \
   } while (0)
-# define SNAPKIT_TRACE_ALWAYS_(d)               SNAPKIT_TRACE_ALWAYS__(d)
-# define SNAPKIT_TRACE_ALWAYS(d)                SNAPKIT_TRACE_ALWAYS_(d)
+#define SNAPKIT_TRACE_ALWAYS_(d, line)  SNAPKIT_TRACE_ALWAYS__(d, line)
+#define SNAPKIT_TRACE_ALWAYS(d)         SNAPKIT_TRACE_ALWAYS_(d, __LINE__)
 /* ========================================================================= */
-# if ((defined(__cplusplus) && (__cplusplus >= 201103L)) ||             \
-      (defined(__STDC__) &&                                             \
-       defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L)))
-#   define SNAPKIT_TRACEF_ALWAYS__(fmt, ...)            \
-  do {                                                  \
-    SNAPKIT_USING_NAMESPACE_STD;                        \
-    fprintf(stderr, "%s(%i) %s : " fmt "\n",            \
-            __FILE__, __LINE__, __func__, __VA_ARGS__); \
-    (void) fflush(stderr);                              \
+#if ((defined(__cplusplus) && (__cplusplus >= 201103L)) ||              \
+     (defined(__STDC__) &&                                              \
+      defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L)))
+# define SNAPKIT_TRACEF_ALWAYS__(line, fmt, ...)                        \
+  do {                                                                  \
+    SNAPKIT_USING_NAMESPACE_STD;                                        \
+    fprintf(stderr, __FILE__ "(" #line ") " __func__ " : " fmt "\n",    \
+            __VA_ARGS__);                                               \
+    (void) fflush(stderr);                                              \
   } while (0)
-#   define SNAPKIT_TRACEF_ALWAYS_(fmt, ...)     \
-  SNAPKIT_TRACEF_ALWAYS__(fmt, __VA_ARGS__)
-#   define SNAPKIT_TRACEF_ALWAYS(fmt, ...)      \
-  SNAPKIT_TRACEF_ALWAYS_(fmt, __VA_ARGS__)
-# endif  /* defined(__STDC__) && ... */
+# define SNAPKIT_TRACEF_ALWAYS_(line, fmt, ...)         \
+  SNAPKIT_TRACEF_ALWAYS__(line, fmt, __VA_ARGS__)
+# define SNAPKIT_TRACEF_ALWAYS(fmt, ...)                \
+  SNAPKIT_TRACEF_ALWAYS_(__LINE__, fmt, __VA_ARGS__)
+#endif  /* defined(__STDC__) && ... */
 /* ========================================================================= */
-# define SNAPKIT_ASSERT_ALWAYS__(c, t, d)       \
+#define SNAPKIT_ASSERT_ALWAYS__(c, t, d)        \
   do { if (!(c)) {                              \
-      SNAPKIT_USING_NAMESPACE_STD;              \
       SNAPKIT_TRACE_ALWAYS(t ": " d);           \
       abort();                                  \
     }                                           \
   } while (0)
-# define SNAPKIT_ASSERT_ALWAYS_(c, t, d)        SNAPKIT_ASSERT_ALWAYS__(c,t,d)
-# define SNAPKIT_ASSERT_ALWAYS(c, d)            SNAPKIT_ASSERT_ALWAYS_(c,#c,d)
+#define SNAPKIT_ASSERT_ALWAYS_(c, t, d)        SNAPKIT_ASSERT_ALWAYS__(c,t,d)
+#define SNAPKIT_ASSERT_ALWAYS(c, d)            SNAPKIT_ASSERT_ALWAYS_(c,#c,d)
 /* ///////////////////////////////////////////////////////////////////////// */
 /* ========================================================================= */
 #if (defined(DEBUG) && !defined(SNAPKIT_DEBUG))
@@ -72,40 +87,9 @@
 #if (defined(NDEBUG) && defined(SNAPKIT_DEBUG))
 # undef SNAPKIT_DEBUG
 #endif
-/* ========================================================================= */
-#define SNAPKIT_VERBOSITY_00                    (0x00)
-#define SNAPKIT_VERBOSITY_01                    (0x3F)
-#define SNAPKIT_VERBOSITY_02                    (0x7F)
-#define SNAPKIT_VERBOSITY_03                    (0xBF)
-#define SNAPKIT_VERBOSITY_04                    (0xFF)
-/* ------------------------------------------------------------------------- */
-#define SNAPKIT_VERBOSITY_DEBUG                 SNAPKIT_VERBOSITY_00
-#define SNAPKIT_VERBOSITY_INFO                  SNAPKIT_VERBOSITY_01
-#define SNAPKIT_VERBOSITY_WARNING               SNAPKIT_VERBOSITY_02
-#define SNAPKIT_VERBOSITY_ERROR                 SNAPKIT_VERBOSITY_03
-#define SNAPKIT_VERBOSITY_CRITICAL              SNAPKIT_VERBOSITY_04
-/* ------------------------------------------------------------------------- */
-#define SNAPKIT_VERBOSITY_ALL                   SNAPKIT_VERBOSITY_DEBUG
-#define SNAPKIT_VERBOSITY_DEFAULT               SNAPKIT_VERBOSITY_INFO
-#define SNAPKIT_VERBOSITY_QUIET                 SNAPKIT_VERBOSITY_CRITICAL
 /* ///////////////////////////////////////////////////////////////////////// */
-#ifdef SNAPKIT_DEBUG
-/* ------------------------------------------------------------------------- */
-# if !defined(SNAPKIT_VERBOSITY)
-#   define SNAPKIT_VERBOSITY                    SNAPKIT_VERBOSITY_DEFAULT
-# endif
 /* ========================================================================= */
-# ifdef __cplusplus
-#   include <cstdio>
-#   include <cstdlib>
-#   include <cstdarg>
-#   define SNAPKIT_USING_NAMESPACE_STD using namespace std
-# else  /* !__cplusplus */
-#   include <stdio.h>
-#   include <stdlib.h>
-#   include <stdarg.h>
-#   define SNAPKIT_USING_NAMESPACE_STD
-# endif  /* !__cplusplus */
+#ifdef SNAPKIT_DEBUG
 /* ========================================================================= */
 # define SNAPKIT_TRACE(d)                       SNAPKIT_TRACE_ALWAYS(d)
 /* ------------------------------------------------------------------------- */
@@ -127,9 +111,9 @@
       SNAPKIT_VERBOSITY <= SNAPKIT_VERBOSITY_INFO)
 #   define SNAPKIT_TRACE_INFO__(d)              SNAPKIT_TRACE(d)
 #   define SNAPKIT_TRACE_INFO_(d)               SNAPKIT_TRACE_INFO__(d)
-#   define SNAPKIT_TRACE_INFO(d)                        SNAPKIT_TRACE_INFO_(d)
+#   define SNAPKIT_TRACE_INFO(d)                SNAPKIT_TRACE_INFO_(d)
 # else
-#   define SNAPKIT_TRACE_INFO(d)                        ((void)(0))
+#   define SNAPKIT_TRACE_INFO(d)                ((void)(0))
 # endif
 /* ------------------------------------------------------------------------- */
 # if (defined(SNAPKIT_VERBOSITY) &&                     \
@@ -180,7 +164,7 @@
 #     define SNAPKIT_TRACEF_DEBUG_(fmt, ...)    \
   SNAPKIT_TRACEF_DEBUG__(fmt, __VA_ARGS__)
 #     define SNAPKIT_TRACEF_DEBUG(fmt, ...)     \
-  SNAPKIT_TRACEF_DEBUG_(fmt, __VA_ARGS__)
+    SNAPKIT_TRACEF_DEBUG_(fmt, __VA_ARGS__)
 #   else
 #     define SNAPKIT_TRACEF_DEBUG(fmt, ...)     ((void)(0))
 #   endif
@@ -190,7 +174,7 @@
 #     define SNAPKIT_TRACEF_INFO__(fmt, ...)    \
   SNAPKIT_TRACEF(fmt, __VA_ARGS__)
 #     define SNAPKIT_TRACEF_INFO_(fmt, ...)     \
-    SNAPKIT_TRACEF_INFO__(fmt, __VA_ARGS__)
+  SNAPKIT_TRACEF_INFO__(fmt, __VA_ARGS__)
 #     define SNAPKIT_TRACEF_INFO(fmt, ...)      \
   SNAPKIT_TRACEF_INFO_(fmt, __VA_ARGS__)
 #   else
@@ -202,7 +186,7 @@
 #     define SNAPKIT_TRACEF_WARNING__(fmt, ...) \
   SNAPKIT_TRACEF(fmt, __VA_ARGS__)
 #     define SNAPKIT_TRACEF_WARNING_(fmt, ...)  \
-    SNAPKIT_TRACEF_WARNING__(fmt, __VA_ARGS__)
+  SNAPKIT_TRACEF_WARNING__(fmt, __VA_ARGS__)
 #     define SNAPKIT_TRACEF_WARNING(fmt, ...)   \
   SNAPKIT_TRACEF_WARNING_(fmt, __VA_ARGS__)
 #   else
@@ -239,7 +223,7 @@
 # define SNAPKIT_ASSERT_VERBOSITY__(l, c, d)                            \
   do { if (SNAPKIT_VERBOSITY <= (l)) { SNAPKIT_ASSERT(c, d); } } while (0)
 # define SNAPKIT_ASSERT_VERBOSITY_(l, c, d)     \
-  SNAPKIT_ASSERT_VERBOSITY__(l, c, d)
+    SNAPKIT_ASSERT_VERBOSITY__(l, c, d)
 # define SNAPKIT_ASSERT_VERBOSITY(l, c, d)      \
   SNAPKIT_ASSERT_VERBOSITY_(l, c, d)
 /* ------------------------------------------------------------------------- */
